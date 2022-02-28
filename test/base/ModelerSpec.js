d0a5a544db69fe624f3fe2b17f5baf45af8077d6
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2022-02-28 09:39:40 +0100

    test: add single-start tests

--- test/base/ModelerSpec.js
+++ test/base/ModelerSpec.js
@@ -1,7 +1,30 @@
+import MochaTestContainer from 'mocha-test-container-support/lib/MochaTestContainer';
+
+import Modeler from '../../lib/base/Modeler';
+import '../helper';
+
+import diagramXML from '../fixtures/simple.dmn';
+
+const singleStart = window.__env__ && window.__env__.SINGLE_START === 'base-modeler';
+
+
 describe('BaseModeler', function() {
 
-  it('should import DMN', function() {
+  let container;
 
-    // TODO(barmac): implement
+  beforeEach(function() {
+    container = MochaTestContainer.get(this);
+  });
+
+  (singleStart ? it.only : it)('should import DMN', async function() {
+
+    // given
+    const modeler = new Modeler({ container });
+
+    // when
+    const { warnings } = await modeler.importXML(diagramXML);
+
+    // then
+    expect(warnings).to.be.empty;
   });
 });
