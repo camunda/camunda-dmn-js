8a1a1efef810dd5d198f2bf0c9f5e84002523540
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-02-09 18:02:56 +0100

    test: simplify distro tests

--- test/distro/helper.js
+++ test/distro/helper.js
@@ -1,47 +1,35 @@
+function distroTest(DmnJS) {
 
-function testImport(DmnModeler, done) {
+  it('should expose globals', function() {
 
-  var container = document.createElement('div');
-  container.style.height = '500px';
-  container.style.border = 'solid 1px #666';
+    if (!DmnJS) {
+      throw new Error('global not bound');
+    }
+  });
 
-  document.body.appendChild(container);
 
-  get('/base/test/fixtures/diagram.dmn', function(err, text) {
+  it('should import initial diagram', async function() {
 
-    if (err) {
-      return done(err);
+    var container = document.createElement('div');
+    container.style.height = '500px';
+    container.style.border = 'solid 1px #666';
+
+    document.body.appendChild(container);
+
+    const response = await fetch('/base/test/fixtures/diagram.dmn');
+
+    if (!response.ok) {
+      throw new Error('failed to fetch diagram');
     }
 
-    var modeler = new DmnModeler({ container: container });
+    const text = await response.text();
 
-    modeler.importXML(text, function(err, warnings) {
-      return done(err, warnings, modeler);
-    });
+    var dmnJS = new DmnJS({ container: container });
+
+    return dmnJS.importXML(text);
   });
 
 }
 
-function get(url, done) {
-  var httpRequest = new XMLHttpRequest();
 
-  if (!httpRequest) {
-    return done(new Error('cannot create XMLHttpRequest'));
-  }
-
-  httpRequest.onreadystatechange = checkDone;
-  httpRequest.open('GET', url);
-  httpRequest.send();
-
-  function checkDone() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        return done(null, httpRequest.responseText);
-      } else {
-        return done(new Error('status = ' + httpRequest.status), null, httpRequest);
-      }
-    }
-  }
-}
-
-window.testImport = testImport;
+window.distroTest = distroTest;
