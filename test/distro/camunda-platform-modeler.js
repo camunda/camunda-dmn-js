0e00a449c8874bb7604e82e0d47ea33cbf493244
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-02-09 18:02:56 +0100

    test: simplify distro tests

--- test/distro/camunda-platform-modeler.js
+++ test/distro/camunda-platform-modeler.js
@@ -1,22 +1,6 @@
 describe('camunda-platform-modeler', function() {
 
-  it('should expose globals', function() {
-
-    var DmnModeler = window.DmnModeler;
-
-    // then
-    expect(DmnModeler).to.exist;
-    expect(new DmnModeler()).to.exist;
-  });
-
-
-  it('should import initial diagram', function(done) {
-
-    var DmnModeler = window.DmnModeler;
-
-    // then
-    /* global testImport */
-    testImport(DmnModeler, done);
-  });
+  /* global distroTest */
+  distroTest(window.DmnModeler);
 
 });
